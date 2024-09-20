2408d9776ea629acf314a9e467117d3da31cdd3a
hithaysha <104015375+hithaysha@users.noreply.github.com>
2024-09-20 22:29:56 +0530

    Initial commit

---
+++ src/MathsUtils.java
@@ -0,0 +1,32 @@
+public class MathsUtils {
+
+    /*Whole Number Calculations*/
+
+    public static int addInt(int a, int b) {
+        return a+b;
+    }
+    public static int subInt(int a, int b) {
+        return a-b;
+    }
+    public static int multiplyInt(int a, int b) {
+        return a*b;
+    }
+    public static int divideInt(int a, int b) {
+        return a/b;
+    }
+
+    /*Floating Point Number Calculations*/
+
+    public static double addDouble(double a, double b) {
+        return a+b;
+    }
+    public static double subDouble(double a, double b) {
+        return a-b;
+    }
+    public static double multiplyDouble(double a, double b) {
+        return a*b;
+    }
+    public static double divideDouble(double a, double b) {
+        return a/b;
+    }
+}
---
+++ src/Var.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Var {
+    /*Standard Code Variables*/
+    public static String MyName = "Hiranji";
+    public static int NameLength = MyName.length();
+    public static int MaxNameLength = 16;
+    public static int NumberOne = 12;
+    public static int NumberTwo = 13;
+    public static int total = MathsUtils.addInt(NumberOne,NumberTwo);
+    public static int CharactersRemaining = MathsUtils.subInt(MaxNameLength,NameLength);
+    public static int FriendCount = 12;
+    public static int ClassStudentCount = 36;
+    public static double DoubleFriendCount = 1.2;
+    public static double DoubleClassStudentCount = 36.9;
+    public static int NonFriendCount = MathsUtils.subInt(ClassStudentCount, FriendCount);
+    public static double DoubleNonFriendCount = MathsUtils.subDouble(DoubleClassStudentCount, DoubleFriendCount);
+
+    /*Print Code Variables*/
+    public static String PeopleDatabaseRun =
+            "The amount of People you know is " + Var.FriendCount + "\n" +
+            "The amount of People In the Class is " + Var.ClassStudentCount + "\n" +
+            "The amount of People you don't know is " + Var.NonFriendCount;
+    public static String DoublesPeopleDatabaseRun =
+            "The amount of People you know is " + Var.DoubleFriendCount + "\n" +
+                    "The amount of People In the Class is " + Var.DoubleClassStudentCount + "\n" +
+                    "The amount of People you don't know is " + Var.DoubleNonFriendCount;
+    public static String AdditionCode =
+            NumberOne + " + " + NumberTwo + " = " + total;
+    public static String NameInfo =
+            "Your name is " + MyName + " which Contains " + NameLength + " characters." + "\n" +
+            "The Maximum Name Length is " + MaxNameLength + " characters and you have " + CharactersRemaining + " characters Remaining";
+}
